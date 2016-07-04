84d09d4dd49089a9f37afe5f7e25418b45baac23
Kristoffer Skaret <kristoffer.skaret@kantega.no>
2016-07-04 10:04:24 +0200

    Simple executable jar logging hello world

---
+++ src/main/java/no/kriska/samples/javadocker/Main.java
@@ -0,0 +1,16 @@
+package no.kriska.samples.javadocker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by kristofferskaret on 04.07.2016.
+ */
+public class Main {
+
+   private static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello docker!!!");
+    }
+}
